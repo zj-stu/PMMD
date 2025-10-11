dee860bd542ff5798258e3225d9c6abf346fe91c
zj-stu <2794577413@qq.com>
2025-10-11 18:40:18 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,18 @@
+EXPID=log20250731_161708
+
+HOST='127.0.0.1'
+PORT='1'
+
+NUM_GPU=1
+
+python test.py \
+--config 'configs/test_DGM4.yaml' \
+--output_dir 'results' \
+--launcher pytorch \
+--rank 0 \
+--log_num ${EXPID} \
+--dist-url tcp://${HOST}:1003${PORT} \
+--token_momentum \
+--world_size $NUM_GPU \
+--test_epoch best \
+
