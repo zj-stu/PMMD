8afd959c90d5ccb823c3c619b1cac08546d479e1
zj-stu <2794577413@qq.com>
2025-10-11 17:18:42 +0800

    Add files via upload

---
+++ dataset/tokenizers/__init__.py
@@ -0,0 +1,29 @@
+from transformers import BertTokenizer, RobertaTokenizer, XLMRobertaTokenizer, AutoTokenizer, BertTokenizerFast
+from dataset.tokenizers.bert_tokenizer_with_dropout import BertTokenizerWithDropout
+
+
+def build_tokenizer(text_encoder: str, dropout=0):
+    print(text_encoder)
+    if ('bert-base-uncased' in text_encoder) or ('bert-large-uncased' in text_encoder) :
+        if dropout > 0:
+            tokenizer = BertTokenizerWithDropout.from_pretrained(text_encoder, dropout=dropout)
+        else:
+            tokenizer = BertTokenizerFast.from_pretrained(text_encoder, local_files_only=True)
+
+    
+
+    elif ('xlm-roberta-base' in text_encoder) or ('xlm-roberta-large' in text_encoder):
+        tokenizer = XLMRobertaTokenizer.from_pretrained(text_encoder)
+
+    elif ('roberta-base' in text_encoder) or ('roberta-large' in text_encoder):
+        tokenizer = RobertaTokenizer.from_pretrained(text_encoder)
+
+    else:
+        raise NotImplementedError(f"tokenizer for {text_encoder}")
+
+    # always use cls and sep
+    tokenizer.add_special_tokens({'bos_token': tokenizer.cls_token})
+    tokenizer.add_special_tokens({'eos_token': tokenizer.sep_token})
+
+    return tokenizer
+
